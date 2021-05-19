952b274fc43a0b03813290576f8314b81f760e43
Andrés Milla <andressmilla@gmail.com>
2021-05-19 13:27:10 -0300

    Added youtube url and changed youtube code datatype

--- api/apps/interview/models.py
+++ api/apps/interview/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from urllib.parse import urlunsplit, urlencode
 
 
 class YoutubeVideo(models.Model):
 
-    code = models.URLField()
+    code = models.CharField(max_length=255)
 
     thumbnail = models.URLField()
+
+    @property
+    def url(self):
+        PATH = "/watch"
+        SCHEME = "https"
+        NETLOC = "www.youtube.com"
+
+        query = urlencode(dict(v=self.code))
+
+        return urlunsplit((SCHEME, NETLOC, PATH, query, ""))
 
 
 class Interview(models.Model):
--- api/apps/interview/serializers.py
+++ api/apps/interview/serializers.py
@@ -1,9 +1,18 @@
-from interview.models import Interview
+from interview.models import Interview, YoutubeVideo
 from rest_framework import serializers
 
 
+class YoutubeVideoSerializer(serializers.ModelSerializer):
+    url = serializers.URLField(read_only=True)
+
+    class Meta:
+        model = YoutubeVideo
+        fields = ['code', 'thumbnail', 'url']
+
+
 class InterviewSerializer(serializers.ModelSerializer):
+    youtube_video = YoutubeVideoSerializer()
+
     class Meta:
         model = Interview
-        fields = '__all__'
-        depth = 1
+        fields = ['name', 'youtube_video']
