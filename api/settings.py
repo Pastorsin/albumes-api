dd99a92c7e3850b7535a0d01a11f379be530b03a
Andrés Milla <andressmilla@gmail.com>
2021-06-05 00:28:16 -0300

    Added django-corsheader to middleware, installed apps and requirements

--- api/settings.py
+++ api/settings.py
@@ -45,6 +45,7 @@ DJANGO_APPS = [
 
 THIRD_APPS = [
     'rest_framework',
+    'corsheaders',
 ]
 
 MY_APPS = [
@@ -62,6 +63,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'api.urls'
