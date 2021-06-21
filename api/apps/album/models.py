49e973215918538dfe8d162bb7959c6680a0c8f0
Andrés Milla <andressmilla@gmail.com>
2021-06-21 19:22:10 -0300

    Added soft deleted albums to admin panel

--- api/apps/album/models.py
+++ api/apps/album/models.py
@@ -5,12 +5,13 @@ from model_utils.models import SoftDeletableModel, TimeStampedModel
 
 
 class Album(TimeStampedModel, SoftDeletableModel):
-
     class Corner(models.TextChoices):
         TOP_LEFT = 'TOP_LEFT', _('Top left')
         TOP_RIGHT = 'TOP_RIGHT', _('Top right')
         BOTTOM_LEFT = 'BOTTOM_LEFT', _('Bottom left')
         BOTTOM_RIGHT = 'BOTTOM_RIGHT', _('Bottom right')
+
+    all_objects = models.Manager()
 
     name = models.CharField(max_length=255)
 
