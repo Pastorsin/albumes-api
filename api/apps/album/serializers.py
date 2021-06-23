ddcf766f458471999fa639b2218063cd8c81039f
Andrés Milla <andressmilla@gmail.com>
2021-06-23 08:21:16 -0300

    Added only interview ids for write operations

--- api/apps/album/serializers.py
+++ api/apps/album/serializers.py
@@ -1,15 +1,23 @@
-from album.models import Album
+from album.models import Album, Interview
 from interview.serializers import InterviewSerializer
 from rest_framework import serializers
 
 
 class AlbumSerializer(serializers.ModelSerializer):
-    interviews = InterviewSerializer(many=True)
+    interviews = InterviewSerializer(many=True, read_only=True)
+
+    interview_ids = serializers.PrimaryKeyRelatedField(
+        many=True,
+        write_only=True,
+        source='interviews',
+        queryset=Interview.objects.all()
+    )
 
     class Meta:
         model = Album
         fields = [
-            'id', 'name', 'qr_position', 'interviews', 'created', 'modified'
+            'id', 'name', 'qr_position', 'interviews', 'interview_ids',
+            'created', 'modified'
         ]
 
 
