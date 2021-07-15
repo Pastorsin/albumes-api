464ac4065fd9c41bf24404b7fda17d59a9cc06d2
Andrés Milla <andressmilla@gmail.com>
2021-07-14 21:46:35 -0300

    Added youtube code validator

--- api/apps/interview/serializers.py
+++ api/apps/interview/serializers.py
@@ -1,5 +1,21 @@
 from interview.models import Interview, YoutubeVideo
+import requests
 from rest_framework import serializers
+
+
+class YoutubeCodeValidator():
+    MESSAGE = "Code of Youtube video is incorrect."
+
+    def __call__(self, video):
+        code = video["code"]
+
+        video_url = f"http://www.youtube.com/watch?v={code}"
+        check_url = f"https://www.youtube.com/oembed?url={video_url}"
+
+        response = requests.head(check_url)
+
+        if not response.ok:
+            raise serializers.ValidationError(self.MESSAGE)
 
 
 class YoutubeVideoSerializer(serializers.ModelSerializer):
@@ -8,6 +24,7 @@ class YoutubeVideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = YoutubeVideo
         fields = ['id', 'code', 'thumbnail', 'url']
+        validators = [YoutubeCodeValidator()]
 
 
 class InterviewSerializer(serializers.ModelSerializer):
